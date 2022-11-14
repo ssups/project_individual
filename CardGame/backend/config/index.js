c1b2b5983d1c484e5addeffbd8c641830b51a9dc
ssups <shalstlqruki@gmail.com>
2022-11-14 22:49:03 +0900

    1114

--- CardGame/backend/config/index.js
+++ CardGame/backend/config/index.js
@@ -4,6 +4,8 @@ const config = {
     password: "Admin123!",
     database: "react_card_game",
     host: "localhost",
+    // host: "127.0.0.1",
+    // port: 3000,
     dialect: "mysql",
     timezone: "+09:00",
   },
